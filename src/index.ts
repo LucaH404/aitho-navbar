d1f39c324aabcd5bbf6c54d937b1bb55a97d6d8b
LucaH404 <luca.sapienza03@gmail.com>
2023-07-31 16:21:20 +0200

    typescript version of previous code

---
+++ src/index.ts
@@ -0,0 +1,136 @@
+// Add necessary typings
+interface Comment {
+    id: number;
+    user: {
+      username: string;
+    };
+    body: string;
+  }
+  
+  // Convert waitHide and waitAlert functions to use promises instead of alerts
+  function waitHide(): Promise<void> {
+    return new Promise((resolve) => {
+      alert("you clicked the english alert ;)");
+      const response = "english";
+      setTimeout(() => {
+        console.log(response);
+        resolve();
+      }, 3000);
+    });
+  }
+  
+  function waitAlert(): Promise<void> {
+    return new Promise((resolve) => {
+      alert("hai premuto il pulsante");
+      const responseAlert = "boo";
+      setTimeout(() => {
+        console.log(responseAlert);
+        resolve();
+      }, 3000);
+    });
+  }
+  
+  // TypeScript equivalent for the remaining code
+  $(function () {
+    $("#bell").on("click", async function () {
+      await waitHide();
+    });
+  });
+  
+  $(function () {
+    $("#mark").on("click", async function () {
+      await waitAlert();
+    });
+  });
+  
+  $(function () {
+    function toggleImg() {
+      var currentSrc = $("img.nav-icon").attr("src");
+  
+      currentSrc === "./icon/aitho-icon.png"
+        ? $("img.nav-icon").attr("src", "./icon/aitho-icon-white.png")
+        : $("img.nav-icon").attr("src", "./icon/aitho-icon.png");
+    }
+  
+    $("#darkmode").on("click", function () {
+      $("#wrapper").toggleClass("wrapper");
+      $("a.nav-link").toggleClass("nav-link-dark");
+      $("#navbar").toggleClass("darkmode");
+      $("#bell").toggleClass("bell-dark");
+      const src = "./icon/aitho-icon-white.png";
+      $("img.nav-icon").attr("src", src);
+      $("#darkmode").click(toggleImg);
+      $("div.cell").toggleClass("cell-dark");
+      $("#comment-section").toggleClass("dark-table");
+    });
+  });
+  
+  let isCalled = false;
+  let limit = 10;
+  let skip = 0;
+  let count = 0;
+  
+  async function fetchAPI(): Promise<void> {
+    try {
+      const url = `https://dummyjson.com/comments?skip=${skip}&limit=${limit}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      const comments: Comment[] = data.comments;
+  
+      const commentArr = comments.map((comment) => {
+        return `
+          <tbody>
+            <tr>
+              <th scope="row">${comment.id}</th>
+              <td>${comment.user.username}</td>
+              <td>${comment.body}</td>
+            </tr>
+          </tbody>
+        `;
+      });
+  
+      const commentsAsString = commentArr.join("");
+      $("#comment-section").append(commentsAsString);
+    } catch (err) {
+      console.log("Request Failed", err);
+    }
+  }
+  
+  const incCount = () => {
+    return count++;
+  };
+  
+  $(function () {
+    $("#comment-event").on("click", function () {
+      if (isCalled === false) {
+        fetchAPI();
+        $("#counter").empty().append(`Lettura blocco: ${count}`);
+        $("#more").empty().append(`Leggi il prossimo blocco  (${count + 1})`);
+        isCalled = true;
+      } else {
+        return null;
+      }
+      $("#comment-section").show();
+      $("div.comment-box").show();
+      setTimeout(() => $("#more").show(), 240);
+    });
+  });
+  
+  const showMore = () => {
+    skip += limit;
+    limit = 10;
+    incCount();
+    fetchAPI();
+  };
+  
+  $(function () {
+    $("div.comment-box").hide();
+    $("#comment-section").hide();
+    $("#more").hide();
+    $("#more").on("click", function () {
+      showMore();
+      $("#counter").empty().append(`Lettura blocco: ${count}`);
+      $("#more").empty().append(`Leggi il prossimo blocco  (${count + 1})`);
+    });
+  });
+  
